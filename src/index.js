866247b4aa81f39a792d707a0e38530b5a41236b
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-06-05 13:06:44 +0200

    gallery expand

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const apiKey = '37042837-efaf1ad73896b324f11be5dd6';
 const search = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const loadBtn = document.querySelector('.load-more');
+const loadBtn = document.querySelector('.load-btn');
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 let lightbox;
-//let page = 1;
+let page = 1;
 let q = '';
 
 async function getUl(userRequest, page) {
@@ -70,7 +70,42 @@ function galleryExpand() {
   page += 1;
   lightbox.destroy();
   q = search.searchQuery.value;
-  getUl(q, page).then(imagesArray => {});
+  getUl(q, page).then(imagesArray => {
+    const pictures = imagesArray.hits
+      .map(
+        gallery => `<a class= "gallery-link" href="${gallery.largeImageURL}"><div class="photo-card">
+        <img src="${gallery.webformatURL}" alt="${gallery.tags}" loading="lazy" />
+        <div class="info">
+        <p class="info-item">
+    <b>Likes</b> ${gallery.likes}
+  </p>
+  <p class="info-item">
+    <b>Views</b> ${gallery.views}
+  </p>
+  <p class="info-item">
+    <b>Comments</b> ${gallery.comments}
+  </p>
+  <p class="info-item">
+    <b>Downloads</b> ${gallery.downloads}
+  </p>
+        </div>
+        </div></a>`
+      )
+      .join('');
+    gallery.insertAdjacentHTML('beforeend', pictures);
+    lightbox = new SimpleLightbox('a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    }).refresh();
+    const pages = Math.ceil(imagesArray.totalHits / 40);
+    if (page >= pages) {
+      loadBtn.classList.add('is-hidden');
+      return Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  });
 }
 
 search.addEventListener('submit', galleryConstructor);
+loadBtn.addEventListener('click', galleryExpand);
